7f33579e8acf4c88709861ce2b93603cce6eb853
BurningS0ul <45243901+BurningS0ul@users.noreply.github.com>
2022-05-05 20:15:13 +0200

    #6 Start

---
+++ Aufgaben/06-Typescript_Info/eu.js
@@ -0,0 +1,5 @@
+const mycountry = ["Deutschland", "Italien", "Frankreich", "Spanien"];
+var year= "2022";
+var population= "90000";
+
+console.log("Die Einwohnerzahl in " + mycountry[0] + " beträgt im Jahr " + year + ": " + population);
