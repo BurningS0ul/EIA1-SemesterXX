686d797a6c141d8051b141ab96d24072291992f7
BurningS0ul <45243901+BurningS0ul@users.noreply.github.com>
2022-05-19 18:28:43 +0200

    #8 Start

---
+++ Aufgaben/08-Instrument/beat.ts
@@ -0,0 +1,54 @@
+
+
+window.addEventListener("load", function(){
+    document.querySelector(".pad-1").addEventListener('click', playSample);
+    document.querySelector(".pad-2").addEventListener('click', playSample);
+    document.querySelector(".pad-3").addEventListener('click', playSample);
+    document.querySelector(".pad-4").addEventListener('click', playSample);
+    document.querySelector(".pad-5").addEventListener('click', playSample);
+    document.querySelector(".pad-6").addEventListener('click', playSample);
+    document.querySelector(".pad-7").addEventListener('click', playSample);
+    document.querySelector(".pad-8").addEventListener('click', playSample);
+    document.querySelector(".pad-9").addEventListener('click', playSample);
+});
+
+function playSample(){    
+    if(".pad-1"){
+        var sound:HTMLAudioElement = new Audio('assets/Drumpad/A.mp3');
+        sound.play();
+    }
+    if(".pad-2"){
+        var sound:HTMLAudioElement = new Audio('assets/Drumpad/C.mp3');
+        sound.play();
+    }
+    if(".pad-3"){
+        var sound:HTMLAudioElement = new Audio('assets/Drumpad/F.mp3');
+        sound.play();
+    }
+    if(".pad-4"){
+        var sound:HTMLAudioElement = new Audio('assets/Drumpad/G.mp3');
+        sound.play();
+    }
+    if(".pad-5"){
+        var sound:HTMLAudioElement = new Audio('assets/Drumpad/hihat.mp3');
+        sound.play();
+    }
+    if(".pad-6"){
+        var sound:HTMLAudioElement = new Audio('assets/Drumpad/kick.mp3');
+        sound.play();
+    }
+    if(".pad-7"){
+        var sound:HTMLAudioElement = new Audio('assets/Drumpad/laugh-1.mp3');
+        sound.play();
+    }
+    if(".pad-8"){
+        var sound:HTMLAudioElement = new Audio('assets/Drumpad/laugh-2.mp3');
+        sound.play();
+    }
+    if(".pad-9"){
+        var sound:HTMLAudioElement = new Audio('assets/Drumpad/snare.mp3');
+        sound.play();
+    }
+}
+
+
