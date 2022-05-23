07233eb6d592b70fd01d55041299e045bc781ae5
BurningS0ul <45243901+BurningS0ul@users.noreply.github.com>
2022-05-23 15:12:52 +0200

    #8.1 & 8.2 Revision

--- Aufgaben/08-Instrument/DrumKit/beat.js
+++ Aufgaben/08-Instrument/DrumKit/beat.js
@@ -1,57 +1,70 @@
-window.addEventListener("load", function () {
-    document.querySelector(".beat").addEventListener('click', playSample);
-    document.addEventListener('keypress', playSample);
-});
-function playSample() {
-    //activate on keypress
+window.addEventListener("load", Clicker);
+window.addEventListener("load", Keypress);
+function Clicker() {
+    document.querySelector(".pad-1").addEventListener('click', function () { playSample("../assets/Drumpad/kick.mp3"); });
+    document.querySelector(".pad-2").addEventListener('click', function () { playSample("../assets/Drumpad/snare.mp3"); });
+    document.querySelector(".pad-3").addEventListener('click', function () { playSample("../assets/Drumpad/hihat.mp3"); });
+    document.querySelector(".pad-4").addEventListener('click', function () { playSample("../assets/Drumpad/F.mp3"); });
+    document.querySelector(".pad-5").addEventListener('click', function () { playSample("../assets/Drumpad/G.mp3"); });
+    document.querySelector(".pad-6").addEventListener('click', function () { playSample("../assets/Drumpad/A.mp3"); });
+    document.querySelector(".pad-7").addEventListener('click', function () { playSample("../assets/Drumpad/C.mp3"); });
+    document.querySelector(".pad-8").addEventListener('click', function () { playSample("../assets/Drumpad/laugh-1.mp3"); });
+    document.querySelector(".pad-9").addEventListener('click', function () { playSample("../assets/Drumpad/laugh-2.mp3"); });
+    document.querySelector(".play").addEventListener('click', function () { playButton(); });
+}
+function playSample(beats) {
+    var sound = new Audio(beats);
+    sound.play();
+}
+function Keypress() {
     document.addEventListener('keydown', (event) => {
         if (event.key == "7") {
-            Pad1();
+            playSample("../assets/Drumpad/kick.mp3");
             document.querySelector(".pad-1").setAttribute("style", "transition: all .3s");
             document.querySelector(".pad-1").setAttribute("style", "transform: scale(0.8)");
         }
         else if (event.key == "8") {
-            Pad2();
+            playSample("../assets/Drumpad/snare.mp3");
             document.querySelector(".pad-2").setAttribute("style", "transition: all .3s");
             document.querySelector(".pad-2").setAttribute("style", "transform: scale(0.8)");
         }
         else if (event.key == "9") {
-            Pad3();
+            playSample("../assets/Drumpad/hihat.mp3");
             document.querySelector(".pad-3").setAttribute("style", "transition: all .3s");
             document.querySelector(".pad-3").setAttribute("style", "transform: scale(0.8)");
         }
         else if (event.key == "4") {
-            Pad4();
+            playSample("../assets/Drumpad/F.mp3");
             document.querySelector(".pad-4").setAttribute("style", "transition: all .3s");
             document.querySelector(".pad-4").setAttribute("style", "transform: scale(0.8)");
         }
         else if (event.key == "5") {
-            Pad5();
+            playSample("../assets/Drumpad/G.mp3");
             document.querySelector(".pad-5").setAttribute("style", "transition: all .3s");
             document.querySelector(".pad-5").setAttribute("style", "transform: scale(0.8)");
         }
         else if (event.key == "6") {
-            Pad6();
+            playSample("../assets/Drumpad/A.mp3");
             document.querySelector(".pad-6").setAttribute("style", "transition: all .3s");
             document.querySelector(".pad-6").setAttribute("style", "transform: scale(0.8)");
         }
         else if (event.key == "1") {
-            Pad7();
+            playSample("../assets/Drumpad/C.mp3");
             document.querySelector(".pad-7").setAttribute("style", "transition: all .3s");
             document.querySelector(".pad-7").setAttribute("style", "transform: scale(0.8)");
         }
         else if (event.key == "2") {
-            Pad8();
+            playSample("../assets/Drumpad/laugh-1.mp3");
             document.querySelector(".pad-8").setAttribute("style", "transition: all .3s");
             document.querySelector(".pad-8").setAttribute("style", "transform: scale(0.8)");
         }
         else if (event.key == "3") {
-            Pad9();
+            playSample("../assets/Drumpad/laugh-2.mp3");
             document.querySelector(".pad-9").setAttribute("style", "transition: all .3s");
             document.querySelector(".pad-9").setAttribute("style", "transform: scale(0.8)");
         }
         else if (event.key == "0") {
-            PlayButton();
+            playButton();
             document.querySelector(".play").setAttribute("style", "transition: all .3s");
             document.querySelector(".play").setAttribute("style", "transform: scale(0.8)");
         }
@@ -68,70 +81,20 @@ function playSample() {
             document.querySelector(".play").setAttribute("style", "transform: ");
         });
     });
-    //Activate on click
-    document.querySelector(".pad-1").addEventListener('click', Pad1);
-    function Pad1() {
-        var sound = new Audio('../assets/Drumpad/kick.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    document.querySelector(".pad-2").addEventListener('click', Pad2);
-    function Pad2() {
-        var sound = new Audio('../assets/Drumpad/snare.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    document.querySelector(".pad-3").addEventListener('click', Pad3);
-    function Pad3() {
-        var sound = new Audio('../assets/Drumpad/hihat.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    document.querySelector(".pad-4").addEventListener('click', Pad4);
-    function Pad4() {
-        var sound = new Audio('../assets/Drumpad/F.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    document.querySelector(".pad-5").addEventListener('click', Pad5);
-    function Pad5() {
-        var sound = new Audio('../assets/Drumpad/G.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    document.querySelector(".pad-6").addEventListener('click', Pad6);
-    function Pad6() {
-        var sound = new Audio('../assets/Drumpad/A.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    document.querySelector(".pad-7").addEventListener('click', Pad7);
-    function Pad7() {
-        var sound = new Audio('../assets/Drumpad/C.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    document.querySelector(".pad-8").addEventListener('click', Pad8);
-    function Pad8() {
-        var sound = new Audio('../assets/Drumpad/laugh-1.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    document.querySelector(".pad-9").addEventListener('click', Pad9);
-    function Pad9() {
-        var sound = new Audio('../assets/Drumpad/laugh-2.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    document.querySelector(".play").addEventListener('click', PlayButton);
-    var sound = [new Audio("../assets/Drumpad/snare.mp3"), new Audio("../assets/Drumpad/kick.mp3"), new Audio("../assets/Drumpad/hihat.mp3")];
-    function PlayButton() {
-        setInterval(function () {
-            sound[0].play();
-            sound[1].play();
-            sound[2].play();
-            sound.volume = 0.2;
-        }, 500);
-    }
+}
+var Sickbeat = [
+    "../assets/Drumpad/kick.mp3",
+    "../assets/Drumpad/snare.mp3",
+    "../assets/Drumpad/hihat.mp3"
+];
+function playButton() {
+    var counter = 0;
+    setInterval(function () {
+        playSample(Sickbeat[counter]);
+        counter++;
+        if (counter == 3) {
+            counter = 0;
+        }
+    }, 500);
 }
 //# sourceMappingURL=beat.js.map
--- Aufgaben/08-Instrument/Piano/piano.js
+++ Aufgaben/08-Instrument/Piano/piano.js
@@ -1,67 +1,83 @@
-window.addEventListener("load", function () {
-    document.querySelector(".piano").addEventListener('click', playSample);
-    document.addEventListener('keypress', playSample);
-});
-function playSample() {
-    //activate on keypress
+window.addEventListener("load", Click);
+window.addEventListener("load", Press);
+function playSample(keys) {
+    var sound = new Audio(keys);
+    sound.play();
+}
+function Click() {
+    document.querySelector(".key-1").addEventListener('click', function () { playSample("../assets/Keyboard/c.mp3"); });
+    document.querySelector(".key-2").addEventListener('click', function () { playSample("../assets/Keyboard/d.mp3"); });
+    document.querySelector(".key-3").addEventListener('click', function () { playSample("../assets/Keyboard/e.mp3"); });
+    document.querySelector(".key-4").addEventListener('click', function () { playSample("../assets/Keyboard/f.mp3"); });
+    document.querySelector(".key-5").addEventListener('click', function () { playSample("../assets/Keyboard/g.mp3"); });
+    document.querySelector(".key-6").addEventListener('click', function () { playSample("../assets/Keyboard/a.mp3"); });
+    document.querySelector(".key-7").addEventListener('click', function () { playSample("../assets/Keyboard/b.mp3"); });
+    document.querySelector(".black-1").addEventListener('click', function () { playSample("../assets/Keyboard/df.mp3"); });
+    document.querySelector(".black-2").addEventListener('click', function () { playSample("../assets/Keyboard/ef.mp3"); });
+    document.querySelector(".black-3").addEventListener('click', function () { playSample("../assets/Keyboard/gf.mp3"); });
+    document.querySelector(".black-4").addEventListener('click', function () { playSample("../assets/Keyboard/af.mp3"); });
+    document.querySelector(".black-5").addEventListener('click', function () { playSample("../assets/Keyboard/bf.mp3"); });
+    document.querySelector(".play").addEventListener('click', function () { playButton(); });
+}
+function Press() {
     document.addEventListener('keydown', (event) => {
         if (event.key == "A" || event.key == "a") {
-            Key1();
+            document.querySelector(".key-1").addEventListener('click', function () { playSample("../assets/Keyboard/c.mp3"); });
             document.querySelector(".key-1").setAttribute("style", "transition: all .3s");
             document.querySelector(".key-1").setAttribute("style", "transform: scale(0.8)");
         }
         else if (event.key == "W" || event.key == "w") {
-            Black1();
+            document.querySelector(".black-1").addEventListener('click', function () { playSample("../assets/Keyboard/df.mp3"); });
             document.querySelector(".black-1").setAttribute("style", "transition: all .3s");
             document.querySelector(".black-1").setAttribute("style", "transform: scale(0.8)");
         }
         else if (event.key == "S" || event.key == "s") {
-            Key2();
+            document.querySelector(".key-2").addEventListener('click', function () { playSample("../assets/Keyboard/d.mp3"); });
             document.querySelector(".key-2").setAttribute("style", "transition: all .3s");
             document.querySelector(".key-2").setAttribute("style", "transform: scale(0.8)");
         }
         else if (event.key == "E" || event.key == "e") {
-            Black2();
+            document.querySelector(".black-2").addEventListener('click', function () { playSample("../assets/Keyboard/ef.mp3"); });
             document.querySelector(".black-2").setAttribute("style", "transition: all .3s");
             document.querySelector(".black-2").setAttribute("style", "transform: scale(0.8)");
         }
         else if (event.key == "D" || event.key == "d") {
-            Key3();
+            document.querySelector(".key-3").addEventListener('click', function () { playSample("../assets/Keyboard/e.mp3"); });
             document.querySelector(".key-3").setAttribute("style", "transition: all .3s");
             document.querySelector(".key-3").setAttribute("style", "transform: scale(0.8)");
         }
         else if (event.key == "F" || event.key == "f") {
-            Key4();
+            document.querySelector(".key-4").addEventListener('click', function () { playSample("../assets/Keyboard/f.mp3"); });
             document.querySelector(".key-4").setAttribute("style", "transition: all .3s");
             document.querySelector(".key-4").setAttribute("style", "transform: scale(0.8)");
         }
         else if (event.key == "R" || event.key == "r") {
-            Black3();
+            document.querySelector(".black-3").addEventListener('click', function () { playSample("../assets/Keyboard/gf.mp3"); });
             document.querySelector(".black-3").setAttribute("style", "transition: all .3s");
             document.querySelector(".black-3").setAttribute("style", "transform: scale(0.8)");
         }
         else if (event.key == "G" || event.key == "g") {
-            Key5();
+            document.querySelector(".key-5").addEventListener('click', function () { playSample("../assets/Keyboard/g.mp3"); });
             document.querySelector(".key-5").setAttribute("style", "transition: all .3s");
             document.querySelector(".key-5").setAttribute("style", "transform: scale(0.8)");
         }
         else if (event.key == "T" || event.key == "t") {
-            Black4();
+            document.querySelector(".black-4").addEventListener('click', function () { playSample("../assets/Keyboard/af.mp3"); });
             document.querySelector(".black-4").setAttribute("style", "transition: all .3s");
             document.querySelector(".black-4").setAttribute("style", "transform: scale(0.8)");
         }
         else if (event.key == "H" || event.key == "h") {
-            Key6();
+            document.querySelector(".key-6").addEventListener('click', function () { playSample("../assets/Keyboard/a.mp3"); });
             document.querySelector(".key-6").setAttribute("style", "transition: all .3s");
             document.querySelector(".key-6").setAttribute("style", "transform: scale(0.8)");
         }
         else if (event.key == "Z" || event.key == "z") {
-            Black5();
+            document.querySelector(".black-5").addEventListener('click', function () { playSample("../assets/Keyboard/bf.mp3"); });
             document.querySelector(".black-5").setAttribute("style", "transition: all .3s");
             document.querySelector(".black-5").setAttribute("style", "transform: scale(0.8)");
         }
         else if (event.key == "J" || event.key == "j") {
-            Key7();
+            document.querySelector(".key-7").addEventListener('click', function () { playSample("../assets/Keyboard/b.mp3"); });
             document.querySelector(".key-7").setAttribute("style", "transition: all .3s");
             document.querySelector(".key-7").setAttribute("style", "transform: scale(0.8)");
         }
@@ -80,111 +96,29 @@ function playSample() {
             document.querySelector(".key-7").setAttribute("style", "transform: ");
         });
     });
-    //Activate on click
-    document.querySelector(".key-1").addEventListener('click', Key1);
-    function Key1() {
-        var sound = new Audio('../assets/Keyboard/c.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    document.querySelector(".black-1").addEventListener('click', Black1);
-    function Black1() {
-        var sound = new Audio('../assets/Keyboard/df.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    document.querySelector(".key-2").addEventListener('click', Key2);
-    function Key2() {
-        var sound = new Audio('../assets/Keyboard/d.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    document.querySelector(".black-2").addEventListener('click', Black2);
-    function Black2() {
-        var sound = new Audio('../assets/Keyboard/ef.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    document.querySelector(".key-3").addEventListener('click', Key3);
-    function Key3() {
-        var sound = new Audio('../assets/Keyboard/e.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    document.querySelector(".key-4").addEventListener('click', Key4);
-    function Key4() {
-        var sound = new Audio('../assets/Keyboard/f.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    document.querySelector(".black-3").addEventListener('click', Black3);
-    function Black3() {
-        var sound = new Audio('../assets/Keyboard/gf.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    document.querySelector(".key-5").addEventListener('click', Key5);
-    function Key5() {
-        var sound = new Audio('../assets/Keyboard/g.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    document.querySelector(".black-4").addEventListener('click', Black4);
-    function Black4() {
-        var sound = new Audio('../assets/Keyboard/af.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    document.querySelector(".key-6").addEventListener('click', Key6);
-    function Key6() {
-        var sound = new Audio('../assets/Keyboard/a.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    document.querySelector(".black-5").addEventListener('click', Black5);
-    function Black5() {
-        var sound = new Audio('../assets/Keyboard/bf.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    document.querySelector(".key-7").addEventListener('click', Key7);
-    function Key7() {
-        var sound = new Audio('../assets/Keyboard/b.mp3');
-        sound.play();
-        sound.volume = 0.2;
-    }
-    var sound = [
-        new Audio("../assets/Keyboard/a.mp3"),
-        new Audio("../assets/Keyboard/af.mp3"),
-        new Audio("../assets/Keyboard/b.mp3"),
-        new Audio("../assets/Keyboard/bf.mp3"),
-        new Audio("../assets/Keyboard/c.mp3"),
-        new Audio("../assets/Keyboard/d.mp3"),
-        new Audio("../assets/Keyboard/df.mp3"),
-        new Audio("../assets/Keyboard/e.mp3"),
-        new Audio("../assets/Keyboard/ef.mp3"),
-        new Audio("../assets/Keyboard/f.mp3"),
-        new Audio("../assets/Keyboard/g.mp3"),
-        new Audio("../assets/Keyboard/gf.mp3"),
-    ];
-    document.querySelector(".play").addEventListener('click', PlayButton);
-    function PlayButton() {
-        alert("Volume Warning");
-        setInterval(function () {
-            sound[0].play();
-            sound[1].play();
-            sound[2].play();
-            sound[3].play();
-            sound[4].play();
-            sound[5].play();
-            sound[6].play();
-            sound[7].play();
-            sound[8].play();
-            sound[9].play();
-            sound[10].play();
-            sound[11].play();
-            sound.volume = 0.2;
-        }, 500);
-    }
+}
+var Pianokeys = [
+    "../assets/Keyboard/c.mp3",
+    "../assets/Keyboard/df.mp3",
+    "../assets/Keyboard/d.mp3",
+    "../assets/Keyboard/ef.mp3",
+    "../assets/Keyboard/e.mp3",
+    "../assets/Keyboard/f.mp3",
+    "../assets/Keyboard/gf.mp3",
+    "../assets/Keyboard/g.mp3",
+    "../assets/Keyboard/af.mp3",
+    "../assets/Keyboard/a.mp3",
+    "../assets/Keyboard/bf.mp3",
+    "../assets/Keyboard/b.mp3"
+];
+function playButton() {
+    var counter = 0;
+    setInterval(function () {
+        playSample(Pianokeys[counter]);
+        counter++;
+        if (counter == 11) {
+            counter = 0;
+        }
+    }, 500);
 }
 //# sourceMappingURL=piano.js.map
