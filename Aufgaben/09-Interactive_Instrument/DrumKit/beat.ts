b6925e59baf01d2757e82add4226f6ac95f797eb
Burning_s0ul <45243901+BurningS0ul@users.noreply.github.com>
2022-05-26 18:47:44 +0200

    #9 v0.3

--- Aufgaben/09-Interactive_Instrument/DrumKit/beat.ts
+++ Aufgaben/09-Interactive_Instrument/DrumKit/beat.ts
@@ -1,77 +1,77 @@
 window.addEventListener("load", Clicker);
 window.addEventListener("load", Keypress);
 
-function Clicker(){
-    document.querySelector(".pad-1").addEventListener('click', function(){playSample("../assets/Drumpad/kick.mp3")});          
-    document.querySelector(".pad-2").addEventListener('click', function(){playSample("../assets/Drumpad/snare.mp3")});
-    document.querySelector(".pad-3").addEventListener('click', function(){playSample("../assets/Drumpad/hihat.mp3")});
-    document.querySelector(".pad-4").addEventListener('click', function(){playSample("../assets/Drumpad/F.mp3")});
-    document.querySelector(".pad-5").addEventListener('click', function(){playSample("../assets/Drumpad/G.mp3")});
-    document.querySelector(".pad-6").addEventListener('click', function(){playSample("../assets/Drumpad/A.mp3")});
-    document.querySelector(".pad-7").addEventListener('click', function(){playSample("../assets/Drumpad/C.mp3")});    
-    document.querySelector(".pad-8").addEventListener('click', function(){playSample("../assets/Drumpad/laugh-1.mp3")});
-    document.querySelector(".pad-9").addEventListener('click', function(){playSample("../assets/Drumpad/laugh-2.mp3")});
-    document.querySelector(".play").addEventListener('click', function(){playButton()});
+function Clicker() {
+    document.querySelector(".pad-1").addEventListener("click", function() {playSample("../assets/Drumpad/kick.mp3"); });          
+    document.querySelector(".pad-2").addEventListener("click", function() {playSample("../assets/Drumpad/snare.mp3"); });
+    document.querySelector(".pad-3").addEventListener("click", function() {playSample("../assets/Drumpad/hihat.mp3"); });
+    document.querySelector(".pad-4").addEventListener("click", function() {playSample("../assets/Drumpad/F.mp3"); });
+    document.querySelector(".pad-5").addEventListener("click", function() {playSample("../assets/Drumpad/G.mp3"); });
+    document.querySelector(".pad-6").addEventListener("click", function() {playSample("../assets/Drumpad/A.mp3"); });
+    document.querySelector(".pad-7").addEventListener("click", function() {playSample("../assets/Drumpad/C.mp3"); });    
+    document.querySelector(".pad-8").addEventListener("click", function() {playSample("../assets/Drumpad/laugh-1.mp3"); });
+    document.querySelector(".pad-9").addEventListener("click", function() {playSample("../assets/Drumpad/laugh-2.mp3"); });
+    document.querySelector(".fa-play").addEventListener("click", function() {playButton()});
+    document.querySelector(".fa-random").addEventListener("click", function() {Randomizer()});
 }
 
-function playSample(beats: string){     
+function playSample(beats: string) {     
     var sound: HTMLAudioElement = new Audio(beats);
     sound.play();
 }
 
-function Keypress(){
-    document.addEventListener('keydown', (event) => {                
-        if (event.key == "7"){
+function stopSample(x){
+    x.classList.toggle("fa-stop");
+}
+
+function Keypress() {
+    document.addEventListener("keydown", (event) => {                
+        if (event.key == "7") {
             playSample("../assets/Drumpad/kick.mp3");
             document.querySelector(".pad-1").setAttribute("style", "transition: all .3s"); 
             document.querySelector(".pad-1").setAttribute("style", "transform: scale(0.8)");  
         }        
-        else if (event.key == "8"){
+        else if (event.key == "8") {
             playSample("../assets/Drumpad/snare.mp3");
             document.querySelector(".pad-2").setAttribute("style", "transition: all .3s"); 
             document.querySelector(".pad-2").setAttribute("style", "transform: scale(0.8)");              
         }
-        else if (event.key == "9"){
+        else if (event.key == "9") {
             playSample("../assets/Drumpad/hihat.mp3");
             document.querySelector(".pad-3").setAttribute("style", "transition: all .3s"); 
             document.querySelector(".pad-3").setAttribute("style", "transform: scale(0.8)"); 
         }
-        else if (event.key == "4"){
+        else if (event.key == "4") {
             playSample("../assets/Drumpad/F.mp3");
             document.querySelector(".pad-4").setAttribute("style", "transition: all .3s"); 
             document.querySelector(".pad-4").setAttribute("style", "transform: scale(0.8)"); 
         }
-        else if (event.key == "5"){
+        else if (event.key == "5") {
             playSample("../assets/Drumpad/G.mp3");
             document.querySelector(".pad-5").setAttribute("style", "transition: all .3s"); 
             document.querySelector(".pad-5").setAttribute("style", "transform: scale(0.8)"); 
         }
-        else if (event.key == "6"){
+        else if (event.key == "6") {
             playSample("../assets/Drumpad/A.mp3");
             document.querySelector(".pad-6").setAttribute("style", "transition: all .3s"); 
             document.querySelector(".pad-6").setAttribute("style", "transform: scale(0.8)"); 
         }
-        else if (event.key == "1"){
+        else if (event.key == "1") {
             playSample("../assets/Drumpad/C.mp3");
             document.querySelector(".pad-7").setAttribute("style", "transition: all .3s"); 
             document.querySelector(".pad-7").setAttribute("style", "transform: scale(0.8)"); 
         }
-        else if (event.key == "2"){
+        else if (event.key == "2") {
             playSample("../assets/Drumpad/laugh-1.mp3");
             document.querySelector(".pad-8").setAttribute("style", "transition: all .3s"); 
             document.querySelector(".pad-8").setAttribute("style", "transform: scale(0.8)"); 
         }
-        else if (event.key == "3"){
+        else if (event.key == "3") {
             playSample("../assets/Drumpad/laugh-2.mp3");
             document.querySelector(".pad-9").setAttribute("style", "transition: all .3s"); 
             document.querySelector(".pad-9").setAttribute("style", "transform: scale(0.8)"); 
         }
-        else if (event.key == "0"){
-            playButton();
-            document.querySelector(".play").setAttribute("style", "transition: all .3s"); 
-            document.querySelector(".play").setAttribute("style", "transform: scale(0.8)"); 
-        }
-        document.addEventListener('keyup', (event) => {
+        document.addEventListener("keyup", (event) => {
             document.querySelector(".pad-1").setAttribute("style", "transform: ");
             document.querySelector(".pad-2").setAttribute("style", "transform: ");
             document.querySelector(".pad-3").setAttribute("style", "transform: ");
@@ -81,27 +81,33 @@ function Keypress(){
             document.querySelector(".pad-7").setAttribute("style", "transform: ");
             document.querySelector(".pad-8").setAttribute("style", "transform: ");
             document.querySelector(".pad-9").setAttribute("style", "transform: ");
-            document.querySelector(".play").setAttribute("style", "transform: ");
         });
     });
 }
 
-var Sickbeat:string [] = [
+var Sickbeat: string [] = [
     "../assets/Drumpad/kick.mp3",
     "../assets/Drumpad/snare.mp3",
     "../assets/Drumpad/hihat.mp3"
 ];
-    
-function playButton(){       
-    var counter = 0;
+
+var counter = 0;
+
+function Randomizer() {       
+    for (var i = 0; i < 3; i++){  
+        counter = Math.floor(Math.random() * Sickbeat.length);                   
+    }  
+}    
+
+function playButton() {   
 
     setInterval(function() {                       
         playSample(Sickbeat[counter]);
         counter++;
-        if (counter == 3){
+        if (counter == 3) {
             counter = 0;
         }     
-    }, 500);
+    },          500);
 }    
 
 
